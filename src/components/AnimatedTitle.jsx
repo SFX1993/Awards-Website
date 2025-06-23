d85eda35db3db2f4f021aa88df3e195873b9b3ed
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-06-23 13:02:38 -0500

    working on fixing minor bugs

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,5 +1,7 @@
 import gsap from "gsap";
-import React, { use, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const AnimatedTitle = ({ title, containerClass }) => {
   const containerRef = useRef(null);
@@ -15,7 +17,7 @@ const AnimatedTitle = ({ title, containerClass }) => {
       });
       titleAnimation.to(".animated-word", {
         opacity: 1,
-        transform: "translate3d(0,0,0) rotatey(0deg) rotateX(0deg)",
+        transform: "translate3d(0,0,0) rotateY(0deg) rotateX(0deg)",
         ease: "power2.inOut",
         stagger: 0.02,
       });
@@ -25,12 +27,12 @@ const AnimatedTitle = ({ title, containerClass }) => {
 
   return (
     <div ref={containerRef} className={`animated-title ${containerClass}`}>
-      {title.split("<br/>").map((line, index) => (
+      {title.split("<br />").map((line, index) => (
         <div
           key={index}
           className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
         >
-          {line.split("").map((word, i) => (
+          {line.split(" ").map((word, i) => (
             <span
               key={i}
               className="animated-word"
